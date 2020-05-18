23631bdc5873588c8c1db2a9f84410534e6c7287
himanshu85k <himanshu85k@gmail.com>
2020-05-18 23:11:04 +0530

    migrated to ionic code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,68 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonPage, IonButton } from '@ionic/react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Home.css';
 
+import * as faceapi from 'face-api.js'
+
+const MODEL_URL = process.env.PUBLIC_URL + '/models';
+
+async function loadModels(setLoading: any) {
+  console.log('loading models')
+  setLoading(true)
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+  await faceapi.loadFaceLandmarkModel(MODEL_URL)
+  await faceapi.loadFaceRecognitionModel(MODEL_URL)
+  setLoading(false)
+}
+
+async function recognize() {
+  console.log('detecting')
+  const image = imageRef.current;
+  let fullFaceDescriptions = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+  console.log('detected', fullFaceDescriptions)
+  
+  const canvas: HTMLCanvasElement = drawImageOnCanvas(image, canvasRef.current);
+  drawDetectionsOnCanvas(fullFaceDescriptions, image, canvas); 
+}
+
+function drawImageOnCanvas(image: any, canvas: any): HTMLCanvasElement {
+  canvas.width = image.width;
+  canvas.height = image.height;
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(image, 0, 0);
+  return canvas;
+}
+
+function drawDetectionsOnCanvas(fullFaceDescriptions: any, image: any, canvas: HTMLCanvasElement) {
+  const dimensions = {
+    width: image.width,
+    height: image.height
+  }
+  const resizedDimensions = faceapi.resizeResults(fullFaceDescriptions, dimensions);
+  faceapi.draw.drawDetections(canvas, resizedDimensions);
+}
+
+let imageRef: any;
+let canvasRef: any;
+
 const Home: React.FC = () => {
+
+  imageRef = useRef(null);
+  canvasRef = useRef(null);
+
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadModels(setLoading)
+  }, [])
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        {isLoading ?
+          'Loading...' : <IonButton onClick={recognize}>Choose One</IonButton>}
+        <img src={process.env.PUBLIC_URL + '/sample.jpeg'} alt="" ref={imageRef} />
+        <canvas ref={canvasRef}/>
       </IonContent>
     </IonPage>
   );
